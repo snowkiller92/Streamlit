030e76b199bc91ea33939217fa6fb1006fb73229
snowkiller92 <138618645+snowkiller92@users.noreply.github.com>
2026-01-15 20:40:45 +0200

    testStreamlit.py

--- testStreamlit.py
+++ testStreamlit.py
@@ -3,11 +3,12 @@ from datetime import datetime
 import pytz
 import time
 
-st.title("Μπορώ να αφήσω τον υπολογιστή ανοιχτό για να μην χασω τα chat με το ChatGPT? δεν θελω να κανω account")
+st.title("Μπορώ να αφήσω τον υπολογιστή ανοιχτό για να μην χασω τα chat με το ChatGPT? Δεν θελω να κανω account")
 
 athens_tz = pytz.timezone("Europe/Athens")
 current_time = datetime.now(athens_tz).strftime("%H:%M:%S")
 
 st.write(f"Current time in Athens: {current_time}")
+
 
 
