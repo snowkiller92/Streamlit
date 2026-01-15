e4e4ef4a4d539ea1295463bf7912f9a1d57bac9f
snowkiller92 <138618645+snowkiller92@users.noreply.github.com>
2026-01-15 20:36:02 +0200

    testStreamlit.py

--- testStreamlit.py
+++ testStreamlit.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytz
 import time
 
-st.title("Test Dashboard")
+st.title("I LOVE ERIKA")
 
 athens_tz = pytz.timezone("Europe/Athens")
 current_time = datetime.now(athens_tz).strftime("%H:%M:%S")
@@ -11,4 +11,5 @@ current_time = datetime.now(athens_tz).strftime("%H:%M:%S")
 st.write(f"Current time in Athens: {current_time}")
 
 if st.button("Refresh time"):
+
     st.rerun()
